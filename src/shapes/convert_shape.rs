56520bb506f0f81c1a004d6d5a12c56678e201cf
Ray Britton <raybritton@pm.me>
2022-09-05 11:37:26 +0100

    Add triangle shape converters

--- src/shapes/convert_shape.rs
+++ src/shapes/convert_shape.rs
@@ -4,13 +4,16 @@
 //!
 //! Line -> Rect
 //!
-//! Rect -> Line
+//! Triangle -> Rect
+//!             Circle
 //!
 
+use crate::Coord;
 use crate::shapes::circle::Circle;
 use crate::shapes::line::Line;
 use crate::shapes::rect::Rect;
-use crate::shapes::{DrawType, Shape};
+use crate::shapes::Shape;
+use crate::shapes::triangle::Triangle;
 
 impl From<Circle> for Rect {
     fn from(circle: Circle) -> Self {
@@ -30,12 +33,27 @@ impl From<Line> for Rect {
     }
 }
 
-impl From<Rect> for Line {
-    fn from(rect: Rect) -> Self {
-        let color = match rect.draw_type() {
-            DrawType::Stroke(c) => c,
-            DrawType::Fill(c) => c,
-        };
-        Line::new(rect.points()[0], rect.points()[1], color)
+
+impl From<Triangle> for Rect {
+    fn from(triangle: Triangle) -> Self {
+        let x1 = triangle.leftmost_point().x;
+        let y1 = triangle.topmost_point().y;
+        let x2 = triangle.rightmost_point().x;
+        let y2 = triangle.bottommost_point().y;
+        Rect::new((x1, y1), (x2, y2), triangle.draw_type())
+    }
+}
+
+impl From<Triangle> for Circle {
+    fn from(triangle: Triangle) -> Self {
+        let x1 = triangle.leftmost_point().x;
+        let y1 = triangle.topmost_point().y;
+        let x2 = triangle.rightmost_point().x;
+        let y2 = triangle.bottommost_point().y;
+        let top_left = Coord::new(x1, y1);
+        let bottom_right = Coord::new(x2, y2);
+        let center = top_left.mid_point(bottom_right);
+        let radius = (center.x - x1).max(top_left.y - y1).abs() as usize;
+        Circle::new(center, radius, triangle.draw_type())
     }
 }
