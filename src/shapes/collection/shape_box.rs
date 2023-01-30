d3b04b843d9f5d5b1b53f0f2e6be4a1fb2b90684
Emma Britton <emmabritton@pm.me>
2023-01-30 15:41:40 +0000

    Add bounds and with_draw_type to ShapeBox

--- src/shapes/collection/shape_box.rs
+++ src/shapes/collection/shape_box.rs
@@ -3,15 +3,17 @@ use crate::drawing::Renderable;
 use crate::shapes::CreateDrawable;
 use crate::Graphics;
 use graphics_shapes::circle::Circle;
+use graphics_shapes::coord::Coord;
 use graphics_shapes::ellipse::Ellipse;
 use graphics_shapes::line::Line;
 use graphics_shapes::polygon::Polygon;
 use graphics_shapes::rect::Rect;
 use graphics_shapes::triangle::Triangle;
+use graphics_shapes::Shape;
 
 /// Represents one of the shapes from graphics-shapes so they be used in a collection with
 /// generic or type issues
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum ShapeBox {
     Line(Drawable<Line>),
     Rect(Drawable<Rect>),
@@ -21,12 +23,34 @@ pub enum ShapeBox {
     Polygon(Drawable<Polygon>),
 }
 
+impl ShapeBox {
+    pub(crate) fn with_draw_type(&self, draw_type: DrawType) -> ShapeBox {
+        match self {
+            ShapeBox::Line(d) => ShapeBox::from_drawable(d.with_draw_type(draw_type)),
+            ShapeBox::Rect(d) => ShapeBox::from_drawable(d.with_draw_type(draw_type)),
+            ShapeBox::Triangle(d) => ShapeBox::from_drawable(d.with_draw_type(draw_type)),
+            ShapeBox::Circle(d) => ShapeBox::from_drawable(d.with_draw_type(draw_type)),
+            ShapeBox::Ellipse(d) => ShapeBox::from_drawable(d.with_draw_type(draw_type)),
+            ShapeBox::Polygon(d) => ShapeBox::from_drawable(d.with_draw_type(draw_type)),
+        }
+    }
+}
+
 pub trait FromDrawable<S: Clone> {
     fn from_drawable(drawable: Drawable<S>) -> ShapeBox;
 }
 
 pub trait FromShape<S> {
     fn from_shape(shape: S, draw_type: DrawType) -> ShapeBox;
+}
+
+impl<S: Clone> From<Drawable<S>> for ShapeBox
+where
+    ShapeBox: FromDrawable<S>,
+{
+    fn from(value: Drawable<S>) -> Self {
+        ShapeBox::from_drawable(value)
+    }
 }
 
 macro_rules! from_drawable {
@@ -62,5 +86,38 @@ impl Renderable<ShapeBox> for ShapeBox {
             ShapeBox::Ellipse(d) => graphics.draw(d),
             ShapeBox::Polygon(d) => graphics.draw(d),
         }
+    }
+}
+
+impl ShapeBox {
+    fn bounds(&self) -> Rect {
+        match self {
+            ShapeBox::Line(d) => d.obj().as_rect(),
+            ShapeBox::Rect(d) => d.obj().clone(),
+            ShapeBox::Triangle(d) => d.obj().as_rect(),
+            ShapeBox::Circle(d) => d.obj().as_rect(),
+            ShapeBox::Ellipse(d) => d.obj().as_rect(),
+            ShapeBox::Polygon(d) => d.obj().as_rect(),
+        }
+    }
+
+    pub fn left(&self) -> isize {
+        self.bounds().left()
+    }
+
+    pub fn top(&self) -> isize {
+        self.bounds().top()
+    }
+
+    pub fn bottom(&self) -> isize {
+        self.bounds().bottom()
+    }
+
+    pub fn right(&self) -> isize {
+        self.bounds().right()
+    }
+
+    pub fn center(&self) -> Coord {
+        self.bounds().center()
     }
 }
