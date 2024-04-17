0858c8e6e612ee19ad907ab53d67f7b168a8ad34
Emma Britton <emmabritton@pm.me>
2024-04-17 15:56:16 +0100

    Formatting

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,10 +1,10 @@
-use std::fs;
 use anyhow::Result;
-use graphics_shapes::shape_box::ShapeBox;
 use buffer_graphics_lib::prelude::*;
 use buffer_graphics_lib::renderable_image::RenderableImage;
 use buffer_graphics_lib::renderable_macros::DrawOffset;
 use buffer_graphics_lib::text::PixelFont::*;
+use graphics_shapes::shape_box::ShapeBox;
+use std::fs;
 
 fn main() -> Result<()> {
     let mut buffer = Graphics::create_buffer(400, 400);
@@ -24,11 +24,19 @@ fn draw_some_text(graphics: &mut Graphics) {
 
     //1 directly using `draw_text`
     //this is the least efficient way
-    graphics.draw_text("This is some text", TextPos::Px(20, 20), (WHITE, Standard6x7));
+    graphics.draw_text(
+        "This is some text",
+        TextPos::Px(20, 20),
+        (WHITE, Standard6x7),
+    );
     //draws the string "This is some text" at pixel 20x20 using font Standard6x7 in white
 
     //2 using `draw` with pre measured text
-    let text = Text::new("More example text", TextPos::ColRow(6, 5), (RED, Standard4x5, WrappingStrategy::SpaceBeforeCol(8)));
+    let text = Text::new(
+        "More example text",
+        TextPos::ColRow(6, 5),
+        (RED, Standard4x5, WrappingStrategy::SpaceBeforeCol(8)),
+    );
     graphics.draw(&text);
     //draws the string "More example text" at column 6, row 5 using font Standard4x5 in red, wrapping at the first space before column 14 (6 + 8)
     //columns and rows are based on the font size so ColRow(2,2) for Standard6x7 and Standard4x5 would be in different locations on screen
@@ -63,7 +71,12 @@ fn draw_some_shapes(graphics: &mut Graphics) {
 
     //3 in bulk using a `ShapeCollection`
     let mut collection = ShapeCollection::default();
-    InsertShape::insert(&mut collection, 0, Line::new((14, 12), (78, 23)), stroke(MID_GRAY));
+    InsertShape::insert(
+        &mut collection,
+        0,
+        Line::new((14, 12), (78, 23)),
+        stroke(MID_GRAY),
+    );
     let drawable = Drawable::from_obj(ShapeBox::from(Circle::new((14, 12), 10)), stroke(BLUE));
     InsertDrawable::insert(&mut collection, 0, drawable);
     graphics.draw(&collection);
@@ -83,14 +96,14 @@ fn draw_some_images(graphics: &mut Graphics) -> Result<()> {
 
     //1 Images
     let png = open_image("examples/sample.png")?;
-    graphics.draw_image((40,40), &png);
+    graphics.draw_image((40, 40), &png);
 
     //2 IndexedImages
-    let (ici,_) = IndexedImage::from_file_contents(&fs::read("examples/sample.ici")?)?;
-    graphics.draw_indexed_image((50,50), &ici);
+    let (ici, _) = IndexedImage::from_file_contents(&fs::read("examples/sample.ici")?)?;
+    graphics.draw_indexed_image((50, 50), &ici);
 
     //3 These can also be stored as `RenderableImage` or `RenderableIndexedImage`
-    let image = RenderableImage::new(png, (67,12), DrawOffset::TopLeft);
+    let image = RenderableImage::new(png, (67, 12), DrawOffset::TopLeft);
     image.render(graphics);
 
     Ok(())
@@ -102,6 +115,10 @@ fn draw_reversed(graphics: &mut Graphics) {
     let drawable = Drawable::from_obj(Line::new((0, 0), (100, 100)), stroke(PURPLE));
     drawable.render(graphics);
 
-    let text = Text::new("SAMPLE TEXT", TextPos::px(coord!(60,60)), (MAGENTA, Limited3x5));
+    let text = Text::new(
+        "SAMPLE TEXT",
+        TextPos::px(coord!(60, 60)),
+        (MAGENTA, Limited3x5),
+    );
     text.render(graphics);
 }
